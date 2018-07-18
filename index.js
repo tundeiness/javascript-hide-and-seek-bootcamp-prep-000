296a0682ac59869276ccf9fc1b2bbabcd9d68b5e
tunde Oretade <tundeiness@yahoo.co.uk>
2018-07-18 12:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,27 +23,12 @@ function increaseRankBy(n){
   
   const festree = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   
-<<<<<<< HEAD
   
   for(let i = 0; i < festree.length; i++){
     festree[i].innerHTML = parseInt(festree[i].innerHTML) + n;
  }
  
- return festree
-=======
-  //const listItems = list.children;
-  
- // for(let child in festree ){
-//    let listItems = festree[child].children;
- //   for(let sub in listItems){
- //     listItems[sub].innerHTML = parseInt((listItems[sub]) + n);
-//    }
-//  }
-  
- // for(let i = 0; i < festree.length; i++){
- //   festree[i].innerHTML = parseInt(i + n);
-//  }
->>>>>>> e479645e85206376e4dd83844c788674e550b220
+ return festree;
   
 }
 
