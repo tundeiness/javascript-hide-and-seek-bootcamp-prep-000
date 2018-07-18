35a0bd8ba71c24c800a977cc5e9e96a3ef1fe96c
tunde Oretade <tundeiness@yahoo.co.uk>
2018-07-18 12:22:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+function getFirstSelector(selector){
+  
+  const sel = selector;
+  const fest = document.querySelector(sel);
+    return fest;
+  
+}
+
+
+function nestedTarget(){
+  
+  const festtoo = document.querySelector('#nested .target');
+    return festtoo
+  
+}
+
+
+
+function increaseRankBy(n){
+  
+  
+  const festree = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  
+  for(let i = 0; i < festree.length; i++){
+    festree[i].innerHTML = parseInt(festree[i].innerHTML) + n;
+ }
+ 
+ return festree
+  
+}
+
+
+
+function deepestChild(){
+  
+  const festfor = document.querySelector('#app #grand-node div div div div');
+  
+  
+  return festfor;
+  
+}
+
+
+
